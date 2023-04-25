783f686803c39e78254969736a17daaff3182d8e
SirAnthony <anthony@adsorbtion.org>
2023-04-26 01:15:46 +0500

    Add ui for flight table

--- client/src/ship/Flights.tsx
+++ client/src/ship/Flights.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import * as RB from 'react-bootstrap'
-import * as F from '../Fetcher'
-import Ship from './Ship'
 import {Item, Flight, User, UserType, FlightStatus} from '../common/entity'
 import {List as UList} from '../util/controls'
-import {ControlBar} from '../util/controls'
+import {LocationCol} from '../inventory/Item/components';
+import {ErrorMessage} from '../util/errors';
+import * as util from '../common/util'
 import {default as L, LR} from './locale'
-import { LocationCol } from 'src/inventory/Item/components';
 
 type FlightRowParams = {
     flight: Flight
     user: User
-    //onAction: (flight: Flight)=>void
-    onSignup: (flight: Flight)=>void
-    onDelist: (flight: Flight)=>void
-    onBlock: (flight: Flight)=>void
-    onUnblock: (flight: Flight)=>void
-    onHelp: (flight: Flight)=>void
-    onDeparture: (flight: Flight)=>void
-    onArrival: (flight: Flight)=>void
+    onAction: (action: string, flight: Flight)=>void
 }
 
 function FlightRowShip(params: FlightRowParams){
@@ -38,56 +30,28 @@ function FlightLocationCol(params: FlightRowParams){
 }
 
 function FlightActions(params: FlightRowParams){
-    const {flight, user} = params
+    const {flight, user, onAction} = params
     if (!flight.ship)
-        return <span></span>
-    const col = a=><RB.Col>{a}</RB.Col>
-    const btn = (c, t)=><RB.Button onClick={()=>c()}>{t}</RB.Button>
-    switch (flight.status){
-    case FlightStatus.Docked:
-    {
-        if (user.kind==UserType.Captain)
-            return col(btn(params.onSignup, L('act_flight_signup')))
-        if ([UserType.Guard, UserType.Master])
-            return col(btn(params.onBlock, L('act_flight_block')))
-        break
+        return <RB.Col><span></span></RB.Col>
+    const sp = s=>s.split(' ')
+    const actions = {
+        [UserType.Captain]: sp('delist signup'),
+        [UserType.Guard]: sp('block help'),
+        [UserType.Master]: sp('block delist departure arrival unblock'),
     }
-    case FlightStatus.Waiting:
-    {
-        if (user.kind==UserType.Captain)
-            return col(btn(params.onDelist, L('act_flight_delist')))
-        if (user.kind==UserType.Guard)
-            return col(btn(params.onBlock, L('act_flight_block')))
-        if (user.kind==UserType.Master){
-            return <RB.Col>
-                {btn(params.onBlock, L('act_flight_block'))}
-                {btn(params.onDelist, L('act_flight_delist'))}
-                {btn(params.onDeparture, L('act_flight_departure'))}
-            </RB.Col>
-        }
-        break
+    const statuses = {
+       [FlightStatus.Docked]: sp('block signup'),
+       [FlightStatus.Waiting]: sp('block delist departure'),
+       [FlightStatus.InFlight]: sp('arrival'),
+       [FlightStatus.SOS]: sp('arrival help'),
+       [FlightStatus.Blocked]: sp('unblock'),
     }
-    case FlightStatus.InFlight:
-    {
-        if (user.kind==UserType.Master)
-            return col(btn(params.onArrival, L('act_flight_arrival')))
-        break
-    }
-    case FlightStatus.SOS:
-    {
-        if (user.kind==UserType.Guard)
-            return col(btn(params.onHelp, L('act_flight_help')))
-        if (user.kind==UserType.Master)
-            return col(btn(params.onArrival, L('act_flight_arrival')))
-        break
-    }
-    case FlightStatus.Blocked:
-    {
-        if ([UserType.Guard, UserType.Master])
-            return col(btn(params.onUnblock, L('act_flight_unblock')))
-        break
-    }}
-    return col(<span></span>)
+    const btns = statuses[flight.status].filter(s=>actions[user.kind].includes(s))
+        .map(t=><RB.Button onClick={()=>onAction(t, flight)}>
+            {L(`act_flight_${t}`)}</RB.Button>)
+    return <RB.Col>
+      {btns}
+    </RB.Col>
 }
 
 function FlightRow(params: FlightRowParams) {
@@ -113,44 +77,28 @@ export default class List extends UList<FlightListProps, FlightListState> {
     get fetchUrl() { return `/api/ship/flights` }
     constructor(props){
         super(props);
-        ['Signup', 'Delist', 'Block', 'Unblock', 'Help', 'Arrival',
-         'Departure'].forEach(k=>this[`on${k}`] = this[`on${k}`].bind(this))
+        ['Action'].forEach(k=>this[`on${k}`] = this[`on${k}`].bind(this))
     }
-    async onSignup(flight: Flight){
-
-    }
-    async onDelist(flight: Flight){
-
-    }
-    async onBlock(flight: Flight){
-
-    }
-    async onUnblock(flight: Flight){
-
-    }
-    async onHelp(flight: Flight){
-
-    }
-    async onArrival(flight: Flight){
-
-    }
-    async onDeparture(flight: Flight){
-
+    async onAction(name: string, flight: Flight){
+        this.setState({err: null})
+        const ret = await util.wget(`/api/ship/flight/${name}`, {method: 'PUT'})
+        if (ret.err)
+            this.setState({err: ret.err})
+        this.fetch()
     }
     body(){
-        const {list} = this.state
+        const {list, err} = this.state
         const rows = list.map(l=><FlightRow key={`flight_list_${l._id}`} flight={l} 
-            user={this.props.user} onSignup={this.onSignup} onDelist={this.onDelist}
-            onBlock={this.onBlock} onUnblock={this.onUnblock} onHelp={this.onHelp}
-            onArrival={this.onArrival} onDeparture={this.onDeparture}/>)
-        return [<RB.Row key={'ship_list_title'} className="menu-list-title">
-            <RB.Col></RB.Col>
-            <RB.Col>{L('desc_time')}</RB.Col>
-            <RB.Col>{L('desc_ship')}</RB.Col>
-            <RB.Col>{L('desc_flight_type')}</RB.Col>
-            <RB.Col>{L('desc_flight_location')}</RB.Col>
-            <RB.Col>{L('desc_flight_status')}</RB.Col>
-            <RB.Col></RB.Col>
+            user={this.props.user} onAction={this.onAction} />)
+        return [err && <RB.Row><ErrorMessage field={err} /></RB.Row>,
+        <RB.Row key={'ship_list_title'} className="menu-list-title">
+          <RB.Col></RB.Col>
+          <RB.Col>{L('desc_time')}</RB.Col>
+          <RB.Col>{L('desc_ship')}</RB.Col>
+          <RB.Col>{L('desc_flight_type')}</RB.Col>
+          <RB.Col>{L('desc_flight_location')}</RB.Col>
+          <RB.Col>{L('desc_flight_status')}</RB.Col>
+          <RB.Col></RB.Col>
         </RB.Row>, ...rows]
     }
 }
