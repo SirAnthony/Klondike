9ea9753dea62e416fe11d61959f4b52e63092be8
SirAnthony <anthony@adsorbtion.org>
2023-05-03 19:18:12 +0500

    Fix missing prices

--- src/util/config.ts
+++ src/util/config.ts
@@ -6,7 +6,7 @@ import {Time} from '../util/time'
 export async function get_prices() : Promise<{[k in ResourceType]: number}> {
     const prices : {[k in ResourceType]: number}  = {} as any
     const conf = await ConfigController.get()
-    const index = Math.max(0, Time.segments((conf.time.market||2) * date.ms.HOUR))
+    const index = Math.max(0, Time.segments((conf.time?.market||2) * date.ms.HOUR))
     const res = conf.price.res.slice(0, index+1).pop()
     for (let k in res)
         prices[k] = res[k]
